cc21db4a044212241f6f39dfcf1d479bebf772d2
Ruman Malik <rumanmlk123@gmail.com>
2025-10-24 19:51:19 +0500

    Mongo credentials

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,31 @@ const routes = require("./routes");
 
 main().catch((err) => console.log(err));
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://rumanmlk123_db_user:@f*EiC2.rRA4ZCf@cluster0.u9137br.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 async function main() {
   await mongoose.connect("mongodb://mongo:27017/todos", {
     useUnifiedTopology: true,
