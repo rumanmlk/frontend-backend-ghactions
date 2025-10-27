3a342fd97306ec0d5246994b9ebea215fb69154b
Ruman Malik <rumanmlk123@gmail.com>
2025-10-28 00:21:26 +0500

    Remove version

--- frontend/src/components/todo-form.jsx
+++ frontend/src/components/todo-form.jsx
@@ -30,7 +30,7 @@ const TodoForm = ({ saveTodo }) => {
         <Input
           id="title"
           name="title"
-          placeholder="Enter a title for version 8"
+          placeholder="Enter a title"
           type="text"
           value={title}
           onChange={onTitleChange}
@@ -41,7 +41,7 @@ const TodoForm = ({ saveTodo }) => {
         <Input
           id="description"
           name="description"
-          placeholder="Enter a long n long Description for version 8"
+          placeholder="Enter a Description"
           type="textarea"
           value={description}
           onChange={onDescriptionChange}
