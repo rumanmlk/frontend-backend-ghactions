ecde35ea813fce43845cb9c6f28e5a87edb97b22
Ruman Malik <rumanmlk123@gmail.com>
2025-10-30 00:36:06 +0500

    Create env.sh and modify docker file

--- frontend/env.sh
+++ frontend/env.sh
@@ -1,5 +1,5 @@
-# echo "Injecting runtime environment variables..."
+echo "Injecting runtime environment variables..."
 
-# for file in /usr/share/nginx/html/static/js/*.js; do
-#   sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" $file
-# done
+for file in /usr/share/nginx/html/static/js/*.js; do
+  sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" $file
+done
