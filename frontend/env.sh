d84118c8f15624594ea752099524af85ea8c4b5c
Ruman Malik <rumanmlk123@gmail.com>
2025-10-30 00:53:23 +0500

    Change env.sh

--- frontend/env.sh
+++ frontend/env.sh
@@ -1,5 +1,13 @@
-echo "Injecting runtime environment variables..."
+#!/bin/sh
+for i in $(env | grep APP_)
+do
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
+    # sed All files
+    # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
 
-for file in /usr/share/nginx/html/static/js/*.js; do
-  sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" $file
+    # sed JS and CSS only
+    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" '{}' +
 done
+echo 'done'
