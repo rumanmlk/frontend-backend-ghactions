e676396278b1db6c79ab5956cdfa1d1b5e32d139
Ruman Malik <rumanmlk123@gmail.com>
2025-10-31 02:17:01 +0500

    Place underscores

--- frontend/env.sh
+++ frontend/env.sh
@@ -6,6 +6,6 @@ do
     echo $key=$value
 
     # sed JS and CSS only
-    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__REACT_APP_API_URL__|${value}|g" '{}' +
+    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__${key}__|${value}|g" '{}' +
 done
 echo 'done'
