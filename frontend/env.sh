4f8ab11cb7c1670c314956cd0af4c94f8545dc76
Ruman Malik <rumanmlk123@gmail.com>
2025-10-30 19:47:50 +0500

    Revert to original env.sh

--- frontend/env.sh
+++ frontend/env.sh
@@ -6,6 +6,6 @@ do
     echo $key=$value
     
     # sed JS and CSS only
-    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|REACT_APP_API_URL|${REACT_APP_API_URL}|g" '{}' +
+    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" '{}' +
 done
 echo 'done'
