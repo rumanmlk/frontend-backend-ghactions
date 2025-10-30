e5e84e73c4deb56897edb53d3413e077dad45635
Ruman Malik <rumanmlk123@gmail.com>
2025-10-30 21:39:17 +0500

    Removal of secret cmd

--- frontend/env.sh
+++ frontend/env.sh
@@ -2,8 +2,7 @@
 for i in $(env | grep APP_)
 do
     value=$(echo $i | cut -d '=' -f 2-)
-
-    # sed JS and CSS only
-    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|${value}|${REACT_APP_API_URL}|g" '{}' +
+    
+    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|${value}|${value}|g" '{}' +
 done
 echo 'done'
