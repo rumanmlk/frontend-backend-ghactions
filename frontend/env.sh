d521db3373cd8094a786f7da65e156d449fb206f
Ruman Malik <rumanmlk123@gmail.com>
2025-10-30 19:31:44 +0500

    remove key and value vars

--- frontend/env.sh
+++ frontend/env.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 for i in $(env | grep APP_)
-do
-    key=$(echo $i | cut -d '=' -f 1)
-    value=$(echo $i | cut -d '=' -f 2-)
-    echo $key=$value
-    # sed All files
-    # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
-
+do    
     # sed JS and CSS only
     find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" '{}' +
 done
