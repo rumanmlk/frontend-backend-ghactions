2642fae9c26f29b8de8d6e39196f27fe1d62ce65
Ruman Malik <rumanmlk123@gmail.com>
2025-10-30 20:41:38 +0500

    Remove {} +

--- frontend/env.sh
+++ frontend/env.sh
@@ -4,6 +4,6 @@ do
     value=$(echo $i | cut -d '=' -f 2-)
 
     # sed JS and CSS only
-    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|${value}|${REACT_APP_API_URL}|g" '{}' +
+    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|${value}|${REACT_APP_API_URL}|g"
 done
 echo 'done'
