9e43b32484c9885d7419ab42f8c2f4df1f63cc33
Ruman Malik <rumanmlk123@gmail.com>
2025-10-31 02:14:45 +0500

    Replace key value

--- frontend/env.sh
+++ frontend/env.sh
@@ -6,6 +6,6 @@ do
     echo $key=$value
 
     # sed JS and CSS only
-    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|${key}|${value}|g" '{}' +
+    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__REACT_APP_API_URL__|${value}|g" '{}' +
 done
 echo 'done'
