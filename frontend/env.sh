365e4f0b5389cf2b5224c5a7fe901849c585cbe1
Ruman Malik <rumanmlk123@gmail.com>
2025-10-30 21:41:08 +0500

    Fix value of backend url

--- frontend/env.sh
+++ frontend/env.sh
@@ -3,6 +3,5 @@ for i in $(env | grep APP_)
 do
     value=$(echo $i | cut -d '=' -f 2-)
     
-    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|${value}|${value}|g" '{}' +
+    find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|${value}|${REACT_APP_API_URL}|g" '{}' +
 done
-echo 'done'
